4bd924fa9400bc2ac4a07a264983d8cfde67307a
thinkpad <basilebagudu@gmail.com>
2024-09-28 23:11:06 -0400

    inatall 118n for translation

---
+++ FOODIES-FRONTEND/src/i-18n/i18n.ts
@@ -0,0 +1,16 @@
+// src/plugins/i18n.ts
+import { createI18n } from 'vue-i18n';
+import en from '../locales/en.json';
+import fr from '../locales/fr.json';
+
+// Configuration de l'instance i18n
+const i18n = createI18n({
+  locale: 'fr', // Définissez la langue par défaut
+  fallbackLocale: 'fr', // Langue de secours si la clé n'est pas trouvée
+  messages: {
+    en,
+    fr
+  }
+});
+
+export default i18n;
--- FOODIES-FRONTEND/src/main.ts
+++ FOODIES-FRONTEND/src/main.ts
@@ -2,19 +2,18 @@
 /// <reference types="vue" />
 
 import './assets/base.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from "./App.vue";
+import i18n from '../src/i-18n/i18n';
+import { createPinia } from 'pinia'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
 
+
 const app = createApp(App)
-
+app.use(i18n);                // Utilisez i18n comme plugin
 app.use(createPinia())
-
 app.mount('#app')
 
 
