24a7c4771f6b9f75e0663bdd646b21118952cb09
thinkpad <basilebagudu@gmail.com>
2024-10-08 17:41:37 -0400

    add code to userRoutes

--- FODDIES_BACKEND/src/router/userRoutes.ts
+++ FODDIES_BACKEND/src/router/userRoutes.ts
@@ -1,11 +1,29 @@
-
-// src/routes/userRoutes.ts
-import express from 'express';
-import { getAllUsers, /*createUser*/ } from '../controllers/userController';
+import express, { Request, Response } from 'express';
+import { loginUser, registerUser } from '../database/queries/userQueries';
+import * as userController from '../controllers/userController';
 
 const router = express.Router();
 
-router.get('/', getAllUsers); // Route pour obtenir tous les utilisateurs
-//router.post('/', createUser); // Route pour créer un nouvel utilisateur
+router.post('/registerUser', async (req: Request, res: Response) => {
+  try {
+    const newUser = await registerUser(req.body);
+    res.status(201).json(newUser);
+  } catch (err: any) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Route pour la connexion
+router.post('/login', userController.login);
+/*router.post('/loginUser',*/ async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const token = await loginUser(email, password);
+    res.status(200).json({ token });
+  } catch (err: any) {
+    res.status(400).json({ message: err.message });
+  }
+};
 
 export default router;
